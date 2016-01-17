eb2f6de3829213e0e081082e974ac823e694b20f
jessethach <jessethach@gmail.com>
2016-01-16 16:17:24 -0800

    developed shorthand write head methods

---
+++ test/test_server.js
@@ -0,0 +1,39 @@
+const Router = require(__dirname + '/../ruuter');
+const http = require('http');
+const fs = require('fs');
+const goodHead = require(__dirname + '/../lib/good-head');
+
+var router = new Router();
+var requestCount = 0;
+
+router.get('/', function(req, res) {
+  goodHead(res, 200, 'text/plain', 'test stuff yee');
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  // res.write('Hello world ');
+  return res.end();
+});
+
+router.get('/greet', function(req, res) {
+  goodHead(res, 200, 'text/plain', 'Hello user ');
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  // res.write('Hello user ');
+  return res.end();
+});
+
+router.post('/data/', function(req, res) {
+  var requestFile = '/../data/' + ++requestCount + '.json';
+  var filestream = fs.createWriteStream(__dirname + requestFile);
+  var resBody = '';
+  req.pipe(filestream);
+  req.on('data', (chunk) => {
+    resBody += chunk;
+  });
+  req.on('end', () => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello, user ' + (JSON.parse(resBody).name));
+    return res.end();
+  });
+});
+
+var server = http.createServer(router.route());
+server.listen(3000, () => console.log('server up'));//eslint-disable-line
