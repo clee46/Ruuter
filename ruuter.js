b81bd45ea339ab33acdd606e0bfdaaa40ba52dc9
James Mason <jmason@alumni.reed.edu>
2016-01-20 01:57:13 -0800

    Removed some lines but didn't change any functionality, added the rest of the router tests, thing is now npm installable and, in fact installed on npm: https://www.npmjs.com/package/ruuter seems to work if you install it locally in a project and call 'var x = require('ruuter');'.

--- ruuter.js
+++ ruuter.js
@@ -1,2 +1,11 @@
-var router = require(__dirname + '/lib/router.js');
-var fileNamer = require(__dirname + '/lib/fileNamer.js');
+var Router = require(__dirname + '/lib/router.js'), router = new Router();
+/*
+  Your router route functions go here.  For example, somthing like this...
+  router.get('/urlToYourPage', function(req, res) {
+    //Stuff that goes on when the client requests '/urlToYourPage' from your server.  Write files, send headers, be creative.
+  });
+*/
+exports.server = require('http').createServer(router.route()).listen(3000);
+exports.Router = require(__dirname + '/lib/router.js');
+exports.writeFile = require(__dirname + '/lib/writeFile.js');
+exports.headMessage = require(__dirname + '/lib/headMessage.js');
